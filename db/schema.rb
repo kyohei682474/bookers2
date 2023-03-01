65d662bd87610fefae66463a8d825b517489b1af
kyohei682474 <118451830+kyohei682474@users.noreply.github.com>
2023-03-01 01:12:05 +0000

    bookers3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_22_093127) do
+ActiveRecord::Schema.define(version: 2023_02_24_121537) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
