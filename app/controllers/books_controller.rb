44f442a6f866f5a2abf622870bf2c7164d16ceea
kyohei682474 <118451830+kyohei682474@users.noreply.github.com>
2023-03-11 08:35:37 +0000

    bookers2-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,7 +33,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
   end
+  
   # 投稿データのストロングパラメータ
   private
 
